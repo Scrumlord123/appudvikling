466a430b20db7cf5151f79e5ff13359d6a1f35b2
Sheriffen <mikkelbojstrup@gmail.com>
2022-02-01 11:40:28 +0100

    first commit

---
+++ Opgave3/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace Opgave3
+{
+    class Program
+    /* {
+         static void Main(string[] args)
+         {
+             Console.Write("Skriv et tal");
+             int name = int.Parse (Console.ReadLine());
+
+             Console.Write("skriv et tal igen ");
+             int name2 = int.Parse(Console.ReadLine());
+
+             Console.WriteLine(name + " " + name2);
+
+         } 
+
+
+
+     }
+ }
+    */
+
+
+
+    //Opgave 5
+
+    //1
+
+    /*  {
+
+
+          static void Main(string[] args)
+          {
+              Console.Write("Skriv et tal");
+              int name = int.Parse (Console.ReadLine());
+
+              Console.Write("skriv et tal igen ");
+              int name2 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine(name + "+ " + name2);
+
+              Console.WriteLine(name + name2);
+
+          } 
+
+
+
+      }
+  }
+  */
+
+
+    //Opgave 5
+
+    //2
+
+    /*{
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv et tal");
+            int name = int.Parse(Console.ReadLine());
+
+            Console.Write("skriv et tal igen ");
+            int name2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + "-" + name2);
+
+            Console.WriteLine(name - name2);
+
+        }
+
+
+
+    }
+    } */
+
+
+
+    //Opgave 5
+    //3
+
+    /*{
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv et tal");
+            int name = int.Parse(Console.ReadLine());
+
+            Console.Write("skriv et tal igen ");
+            int name2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + "*" + name2);
+
+            Console.WriteLine(name * name2);
+
+        }
+
+
+
+    }
+    }*/
+
+
+
+    //Opgave 5
+    //4
+
+    /*  {
+
+
+          static void Main(string[] args)
+          {
+              Console.Write("Skriv et tal");
+              int name = int.Parse(Console.ReadLine());
+
+              Console.Write("skriv et tal igen ");
+              int name2 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine(name + "/" + name2);
+
+              Console.WriteLine(name / name2);
+
+          }
+
+
+
+      }
+  }*/
+
+
+    //Opgave 6
+
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv antal grader celcius");
+            double celcius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("dette er " + celcius * 1.8 + 32 + " i fahrenheit");
+
+            Console.WriteLine("dette er " + celcius + 273.15 + " i kelvin");
+
+        }
+
+
+
+    }
+}
