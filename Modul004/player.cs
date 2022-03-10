5b6190d0d7be90a6bd53ccc6d9b8eece92c7c6c7
Scrumlord123 <mikkelbojstrup@gmail.com>
2022-03-10 22:03:44 +0100

    player commit

---
+++ Modul004/player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul004
+{
+    internal class player
+    {
+        private string _name;
+        private int _points;
+
+        public player(string n, int points)
+        {
+            _name = n;
+            points = 0;
+        }
+
+        public int point
+        {
+            get { return _points; }
+        }
+
+            public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        } 
+     }
+
+    }
+
